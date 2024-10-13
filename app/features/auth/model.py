fd0eff0ff9ee669c4b76ac77c3c210830daaf412
EmmanuelOZ <davidozuna84@hotmail.com>
2024-10-13 16:28:48 -0600

    formulario de inicio de sesión

--- app/features/auth/model.py
+++ app/features/auth/model.py
@@ -1,5 +1,8 @@
 from app.db.base_model import BaseModel
 from sqlalchemy import Column, String
+from flask_wtf import FlaskForm
+from wtforms import StringField, EmailField
+from wtforms.validators import DataRequired, Email
 
 
 class User(BaseModel):
@@ -8,3 +11,8 @@ class User(BaseModel):
     username = Column(String(80), unique=True, nullable=False)
     email = Column(String(120), unique=True, nullable=False)
     password = Column(String(1000), nullable=False)
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Correo', validators=[DataRequired(), Email()])
+    password = StringField('Contraseña', validators=[DataRequired()])
--- app/features/auth/routes.py
+++ app/features/auth/routes.py
@@ -1,8 +1,29 @@
-from flask import Blueprint, render_template
+
+from flask import Blueprint, render_template, redirect
+from app.features.auth.model import LoginForm
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.get('/login')
+@auth.get('/login/')
 def login():
-    return render_template('auth/login.jinja')
+    form: LoginForm = LoginForm()
+    context = {
+        "form": form
+    }
+    return render_template('auth/login.jinja', **context)
+
+
+@auth.post('/login/')
+def login_post():
+    return redirect("/")
+
+
+@auth.get('/register/')
+def register():
+    return render_template('auth/register.jinja')
+
+
+@auth.post('/register/')
+def register_post():
+    return redirect("/")
