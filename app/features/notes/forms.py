9d220be5fb82cb88c300962ca95c0a1509e02458
RobertoCrz <robertocarlosnunezcruz9@gmail.com>
2024-10-14 23:00:26 -0600

    listo

--- app/features/notes/forms.py
+++ app/features/notes/forms.py
@@ -2,12 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 
-class CrearNotaForm(FlaskForm):
+class NotaForm(FlaskForm):
     titulo = StringField('Título', validators=[DataRequired()])
     contenido = TextAreaField('Contenido', validators=[DataRequired()])
-    submit = SubmitField('Crear Nota')
-
-class EditarNotaForm(FlaskForm):
-    titulo = StringField('Título', validators=[DataRequired()])
-    contenido = TextAreaField('Contenido', validators=[DataRequired()])
-    submit = SubmitField('Guardar cambios')
+    submit = SubmitField('Guardar')
--- app/features/notes/routes.py
+++ app/features/notes/routes.py
@@ -1,10 +1,9 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for
 from app.features.notes.model import Nota
 from app.db import db
-from app.features.notes.forms import CrearNotaForm, EditarNotaForm 
+from app.features.notes.forms import NotaForm 
 
 notes = Blueprint('notes', __name__)
-
 
 @notes.get('/notes')
 def notes_list():
@@ -14,48 +13,37 @@ def notes_list():
 
 @notes.get('/notes/create')
 def create_note():
-    form = CrearNotaForm()
+    form = NotaForm()
     return render_template('notes/crear.jinja', form=form)
 
 
 @notes.post('/notes/create')
 def guardar_note():
-    form = CrearNotaForm()
-    if form.validate_on_submit(): 
-        titulo = form.titulo.data
-        contenido = form.contenido.data
-
-        nueva_nota = Nota(titulo=titulo, contenido=contenido)
-        
-        nueva_nota.id = 1234 
-        
+    form = NotaForm() 
+    if form.validate_on_submit():
+        nueva_nota = Nota(titulo=form.titulo.data, contenido=form.contenido.data)
         db.session.add(nueva_nota)
         db.session.commit()
-
-
         return redirect(url_for('notes.notes_list'))
-
     return render_template('notes/crear.jinja', form=form) 
+
 
 @notes.get('/notes/editar/<int:id>')
 def editar_nota(id):
     nota = Nota.query.get_or_404(id)
-    form = EditarNotaForm(obj=nota)  
+    form = NotaForm(obj=nota) 
     return render_template('notes/editar.jinja', form=form, nota=nota)
 
 @notes.post('/notes/editar/<int:id>')
 def actualizar_nota(id):
     nota = Nota.query.get_or_404(id)
-    form = EditarNotaForm()
-    
-    if form.validate_on_submit(): 
+    form = NotaForm()  
+    if form.validate_on_submit():
         nota.titulo = form.titulo.data
         nota.contenido = form.contenido.data
         db.session.commit()
         return redirect(url_for('notes.notes_list'))
-
-    return render_template('notes/editar.jinja', form=form, nota=nota) 
-
+    return render_template('notes/editar.jinja', form=form, nota=nota)
 
 @notes.route('/notes/eliminar/<int:id>', methods=['GET', 'POST'])
 def eliminar_nota(id):
