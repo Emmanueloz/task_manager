f2b0e57ec1029a07f709af7d7b0c79c91cbe8b9f
RobertoCrz <robertocarlosnunezcruz9@gmail.com>
2024-10-14 22:02:19 -0600

    mejor

--- app/features/notes/routes.py
+++ app/features/notes/routes.py
@@ -1,53 +1,60 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for
 from app.features.notes.model import Nota
 from app.db import db
-
+from app.features.notes.forms import CrearNotaForm, EditarNotaForm 
 
 notes = Blueprint('notes', __name__)
 
 
-
 @notes.get('/notes')
 def notes_list():
-
     notas = Nota.query.all()
     return render_template('notes/lista.jinja', notas=notas)
 
 
 @notes.get('/notes/create')
 def create_note():
-    return render_template('notes/crear.jinja')
+    form = CrearNotaForm()
+    return render_template('notes/crear.jinja', form=form)
 
 
 @notes.post('/notes/create')
 def guardar_note():
-    titulo =request.form.get('titulo')
-    contenido =request.form.get('contenido')
+    form = CrearNotaForm()
+    if form.validate_on_submit(): 
+        titulo = form.titulo.data
+        contenido = form.contenido.data
 
-    id_notes= 11
+        nueva_nota = Nota(titulo=titulo, contenido=contenido)
+        
+        nueva_nota.id = 1234 
+        
+        db.session.add(nueva_nota)
+        db.session.commit()
 
-    nueva_nota = Nota(id=id_notes, titulo=titulo, contenido=contenido)
 
-    db.session.add(nueva_nota)
-    db.session.commit()
+        return redirect(url_for('notes.notes_list'))
 
-
-    return redirect(url_for('notes.notes_list'))
-
+    return render_template('notes/crear.jinja', form=form) 
 
 @notes.get('/notes/editar/<int:id>')
 def editar_nota(id):
     nota = Nota.query.get_or_404(id)
-    return render_template('notes/editar.jinja', nota=nota)
+    form = EditarNotaForm(obj=nota)  
+    return render_template('notes/editar.jinja', form=form, nota=nota)
 
 @notes.post('/notes/editar/<int:id>')
 def actualizar_nota(id):
     nota = Nota.query.get_or_404(id)
-    nota.titulo = request.form.get('titulo')
-    nota.contenido = request.form.get('contenido')
+    form = EditarNotaForm()
     
-    db.session.commit()
-    return redirect(url_for('notes.notes_list'))
+    if form.validate_on_submit(): 
+        nota.titulo = form.titulo.data
+        nota.contenido = form.contenido.data
+        db.session.commit()
+        return redirect(url_for('notes.notes_list'))
+
+    return render_template('notes/editar.jinja', form=form, nota=nota) 
 
 
 @notes.route('/notes/eliminar/<int:id>', methods=['GET', 'POST'])
@@ -56,4 +63,3 @@ def eliminar_nota(id):
     db.session.delete(nota)
     db.session.commit()
     return redirect(url_for('notes.notes_list'))
-
