ccaca43ebb29be7b9478cfc06ab65639ff2a7e7d
Shadow <mraul7875@gmail.com>
2024-10-16 13:20:49 -0600

    adicion de notas y tareas

--- app/features/dashboard/routes.py
+++ app/features/dashboard/routes.py
@@ -1,15 +1,30 @@
-from flask import Blueprint, render_template
-from ..events.models import Evento
-from app.db import db
+from flask import Blueprint, render_template, flash
 from flask_login import login_required, current_user
+from ..events.models import Evento
+from ..notes.model import Nota # Importamos el modelo Nota
+from app.features.tasks.controller import get_tasks_paginado
 
 dashboard = Blueprint('dashboard', __name__)
 
 @dashboard.route('/')
 @login_required
 def index():
-    # Obtener los eventos del usuario actual
     eventos = Evento.query.filter_by(usuario_id=current_user.id).all()
-    # Extraer títulos, descripciones y el id de los eventos
     eventos_data = [{'id': evento.id, 'titulo': evento.titulo, 'descripcion': evento.descripcion} for evento in eventos]
-    return render_template('dashboard/index.jinja', eventos=eventos_data)
+
+    notas = Nota.query.filter_by(usuario_id=current_user.id).all()
+    notas_data = [{'id': nota.id, 'titulo': nota.titulo, 'contenido': nota.contenido} for nota in notas]
+
+    page = 1
+    per_page = 10
+
+    
+    tasks_page, error = get_tasks_paginado(page, per_page, current_user.id)
+
+    if error:
+        flash(f'Error al obtener las tareas: {error}', 'error')
+        tasks_data = []
+    else:
+        tasks_data = tasks_page.items if tasks_page else []
+
+    return render_template('dashboard/index.jinja', eventos=eventos_data, tasks=tasks_data, notas=notas_data)
