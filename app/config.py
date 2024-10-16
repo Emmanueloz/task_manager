48bb5347b96594fabf033058b7a1db03de17d0b7
alexander1939 <alexander1939>
2024-10-15 23:08:45 -0600

    arreglo de la base de datos

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:tavo@127.0.0.1:3306/tasks'
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv(
         'SQLALCHEMY_TRACK_MODIFICATIONS')
 
