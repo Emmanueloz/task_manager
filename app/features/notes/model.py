abe94686a0d9085ca0a15165f0bd1e532d327350
RobertoCrz <robertocarlosnunezcruz9@gmail.com>
2024-10-16 00:10:56 -0600

    segun esta bien

--- app/features/notes/model.py
+++ app/features/notes/model.py
@@ -1,6 +1,7 @@
 from app.db.base_model import BaseModel
 from sqlalchemy import Column, String, Date, Integer
 from datetime import date
+from app import db
 
 class Nota(BaseModel):
     __tablename__ = 'notas'
@@ -10,3 +11,5 @@ class Nota(BaseModel):
     contenido = Column(String(1000), nullable=False)
     fecha_creacion = Column(Date, default=date.today, nullable=False)
     fecha_modificacion = Column(Date, default=date.today, onupdate=date.today, nullable=False)
+    usuario_id = db.Column(db.Integer, db.ForeignKey(
+        'users.id'), nullable=False)
